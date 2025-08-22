fe10ae2d14ab0937e190fc52b6cfdb0c02ac466f
RAHUL <RAHUL>
2025-08-22 22:00:18 +0530

    Fix render start script fix issue

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,24 @@
-import express from "express"
-import { protectRoute } from "../middleware/auth.middleware.js";
+// import express from "express"
+// import { protectRoute } from "../middleware/auth.middleware.js";
+// import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+
+// const router = express.Router();
+
+// router.get("/users" , protectRoute , getUsersForSidebar)
+// router.get("/:id" , protectRoute , getMessages);
+
+// router.post("/send/:id" , protectRoute , sendMessage)
+
+// export default router;
+
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js"; 
 import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
 
-const router = express.Router();
+const router = express.Router(); 
+router.get("/:id" , protectRoute , getMessages); 
 
-router.get("/users" , protectRoute , getUsersForSidebar)
-router.get("/:id" , protectRoute , getMessages);
+router.post("/send/:id" , protectRoute , sendMessage) 
+router.get("/users" , protectRoute , getUsersForSidebar) 
 
-router.post("/send/:id" , protectRoute , sendMessage)
-
-export default router;
+export default router; 
